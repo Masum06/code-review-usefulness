0c7616dc4f24b176f642ee124b034c8abd55b00a
Masum Hasan <youngladesh@gmail.com>
2020-10-14 17:27:44 +0600

    Update fasttext_embedding.py

--- fasttext_embedding.py
+++ fasttext_embedding.py
@@ -2,7 +2,10 @@
 # pip install gensim==3.6.0
 
 import gensim
-model = gensim.models.Word2Vec.load('fasttext_code_comment/fasttext_code_comment')
+model = gensim.models.Word2Vec.load('model_dir/model_name')
 
+# Get embedding
 print(model['Fix the null pointer exception.'])
+
+# Find similar words in vector space
 print(model.wv.most_similar("pointer"))
